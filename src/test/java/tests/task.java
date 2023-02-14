7f43230e94bac1e56fad9d572901264b785da3ca
Yavuz Semiz <yavuz.s25@gmail.com>
2023-02-14 14:18:00 +0100

    clear public void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -2,10 +2,6 @@ package tests;
 
 public class task {
     public static void main(String[] args) {
-
-    }
-    public void hi(){
-
         System.out.println("hi from intellij");
 
         // something new
@@ -13,12 +9,13 @@ public class task {
 
         // what is the new
         //what is old
-        
+
         System.out.println("hi hi hi ");
-        
+
 
         System.out.println("Hello from Github");
 
         // git fetch --> first to send the code for local repo
     }
+
 }
